b0cb6baf5fad7f736b69d18a358b4c332f29a765
oharelbs <63164183+oharelbs@users.noreply.github.com>
2020-07-24 22:34:31 +0300

    Update createForm.js

--- DEV/createForm.js
+++ DEV/createForm.js
@@ -2,7 +2,7 @@ function createForm() {
 
     window.open("funForm.html", "_blank");
 
-    jQuery.get('D:/GitHub/ePractice/DEV/funForm.html', function(data) {
+    jQuery.get('https://raw.githubusercontent.com/oharelbs/ePractice/master/DEV/funForm.html', function(data) {
        var x = document.getElementById('testme').value;
        alert(x)
     });
